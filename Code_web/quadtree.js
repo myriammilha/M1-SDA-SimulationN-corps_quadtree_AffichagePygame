8ad03d2774e8165508de524cba11dcfe9bcec062
Myriam Milha <myriam.milha@edu.univ-paris13.fr>
2026-01-15 18:23:51 +0100

    feat: add quadtree implementation in JavaScript

---
+++ Code_web/quadtree.js
@@ -0,0 +1,134 @@
+// Représente un 2D point avec les coordonnées x et y.
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+// Représente une région rectangulaire avec x, y, largeur (w), and hauteur (h)
+class Rectangle {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  // Méthode pour vérifier si un point est à l'intérieur du rectangle (contains)
+  contains(point) {
+    return (
+      point.x >= this.x - this.w &&
+      point.x < this.x + this.w &&
+      point.y >= this.y - this.h &&
+      point.y < this.y + this.h
+    );
+  }
+
+  // Méthode pour vérifier si le rectangle intersecte avec un autre rectangle (intersects).
+  intersects(range) {
+    return !(
+      range.x - range.w > this.x + this.w ||
+      range.x + range.w < this.x - this.w ||
+      range.y - range.h > this.y + this.h ||
+      range.y + range.h < this.y - this.h
+    );
+  }
+}
+
+// Représente un noeud quadtree.
+// Méthode de subdivision (subdivide), qui insère des points dans le quadtree (insert), Interroger les points dans une plage donnée (requête) et afficher la structure de quadtree (show).
+class QuadTree {
+  constructor(boundary, n) {
+    this.boundary = boundary;
+    this.capacity = n;
+    this.points = [];
+    this.divided = false;
+  }
+
+  subdivide() {
+    let x = this.boundary.x;
+    let y = this.boundary.y;
+    let w = this.boundary.w;
+    let h = this.boundary.h;
+    let ne = new Rectangle(x + w / 2, y - h / 2, w / 2, h / 2);
+    this.northeast = new QuadTree(ne, this.capacity);
+    let nw = new Rectangle(x - w / 2, y - h / 2, w / 2, h / 2);
+    this.northwest = new QuadTree(nw, this.capacity);
+    let se = new Rectangle(x + w / 2, y + h / 2, w / 2, h / 2);
+    this.southeast = new QuadTree(se, this.capacity);
+    let sw = new Rectangle(x - w / 2, y + h / 2, w / 2, h / 2);
+    this.southwest = new QuadTree(sw, this.capacity);
+    this.divided = true;
+  }
+
+  insert(point) {
+    if (!this.boundary.contains(point)) {
+      return false;
+    }
+
+    if (this.points.length < this.capacity) {
+      this.points.push(point);
+      return true;
+    } else {
+      if (!this.divided) {
+        this.subdivide();
+      }
+      if (this.northeast.insert(point)) {
+        return true;
+      } else if (this.northwest.insert(point)) {
+        return true;
+      } else if (this.southeast.insert(point)) {
+        return true;
+      } else if (this.southwest.insert(point)) {
+        return true;
+      }
+    }
+  }
+
+  query(range, found) {
+    if (!found) {
+      found = [];
+    }
+    if (!this.boundary.intersects(range)) {
+      return;
+    } else {
+      for (let p of this.points) {
+        if (range.contains(p)) {
+          found.push(p);
+        }
+      }
+      if (this.divided) {
+        this.northwest.query(range, found);
+        this.northeast.query(range, found);
+        this.southwest.query(range, found);
+        this.southeast.query(range, found);
+      }
+    }
+    return found;
+  }
+
+  show() {
+    stroke(255);
+    noFill();
+    strokeWeight(1);
+    rectMode(CENTER);
+    rect(
+      this.boundary.x,
+      this.boundary.y,
+      this.boundary.w * 2,
+      this.boundary.h * 2
+    );
+    for (let p of this.points) {
+      strokeWeight(2);
+      point(p.x, p.y);
+    }
+
+    if (this.divided) {
+      this.northeast.show();
+      this.northwest.show();
+      this.southeast.show();
+      this.southwest.show();
+    }
+  }
+}
